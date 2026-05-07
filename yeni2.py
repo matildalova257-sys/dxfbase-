5f8b11254dc813e60492a5816358b3305a638f2b
matildalova257-sys <matildalova257@gmail.com>
2026-05-07 14:42:41 +0300

    Update yeni2.py

--- yeni2.py
+++ yeni2.py
@@ -14,7 +14,7 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL or 'sqlite:///site.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
